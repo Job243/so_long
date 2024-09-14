ab81dc53777b1c1888709bde841b922fc4bd7d2b
JOB <job-mafueni@hotmail.fr>
2024-09-14 02:49:58 +0200

    map checking

--- src/check_map.c
+++ src/check_map.c
@@ -1,51 +1,125 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_map.c                                        :+:      :+:    :+:   */
+/*   check_map_2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmafueni <jmafueni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/23 12:03:29 by jmafueni          #+#    #+#             */
-/*   Updated: 2024/08/09 15:30:15 by jmafueni         ###   ########.fr       */
+/*   Created: 2024/07/23 12:10:04 by jmafueni          #+#    #+#             */
+/*   Updated: 2024/08/09 15:52:55 by jmafueni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	file_exists(const char *filename)
+int	is_rectangular(char **map)
 {
-	int	fd;
+	int	len;
+	int	i;
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Error opening file");
+	if (!map || !map[0])
 		return (0);
+	i = 1;
+	len = ft_strlen(map[0]);
+	while (map[i])
+	{
+		if (ft_strlen(map[i]) != len)
+			return (0);
+		i++;
 	}
-	close(fd);
 	return (1);
 }
 
-char	*read_file(const char *filename)
+int	count_char(t_data *data)
 {
-	int		fd;
-	int		bytes_read;
-	char	*buffer;
+	int	x;
+	int	y;
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (NULL);
-	buffer = malloc(BUFFER_SIZE);
-	if (!buffer)
-		return (NULL);
-	bytes_read = read(fd, buffer, BUFFER_SIZE - 1);
-	if (bytes_read <= 0)
+	data->game->total_item = 0;
+	data->game->total_player = 0;
+	data->game->total_exit = 0;
+	y = 0;
+	while (data->map[y])
 	{
-		free(buffer);
-		close(fd);
-		return (NULL);
+		x = 0;
+		while (data->map[y][x])
+		{
+			if (data->map[y][x] == 'P')
+				data->game->total_player++;
+			else if (data->map[y][x] == 'E')
+				data->game->total_exit++;
+			else if (data->map[y][x] == 'C')
+				data->game->total_item++;
+			x++;
+		}
+		y++;
 	}
-	buffer[bytes_read] = '\0';
-	close(fd);
-	return (buffer);
+	return (data->game->total_exit == 1 && data->game->total_player == 1
+			&& data->game->total_item > 0);
+}
+
+int	is_enclosed_in_wall(char **map, int rows, int cols)
+{
+	int	i;
+
+	if (!map || rows < 1 || cols < 1)
+		return (0);
+	i = 0;
+	while (i < cols)
+	{
+		if (map[0][i] != '1' || map[rows - 1][i] != '1')
+			return (0);
+		i++;
+	}
+	i = 0;
+	while (i < rows)
+	{
+		if (map[i][0] != '1' || map[i][cols - 1] != '1')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	validate_char(char **map)
+{
+	int	x;
+	int y;
+
+	if (!map)
+		return (0);
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x] != '\0')
+		{
+			if (map[y][x] != 'P' && map[y][x] != 'E' && map[y][x] != 'C'
+				&& map[y][x] != '0' && map[y][x] != '1')
+				return (0);
+			x++;
+		}
+		y++;
+	}
+	return (1);
+}
+
+int	validate_map(t_data *data)
+{
+	int	cols;
+	int	rows;
+	
+	rows = 0;
+	while (data->map[rows])
+		rows++;
+	cols = ft_strlen(data->map[0]);
+	if (!is_rectangular(data->map))
+		return (map_error("Error: Map is not rectangular.\n", data), 0);
+	if (!is_enclosed_in_wall(data->map, rows, cols))
+		return (map_error("Error : Map is not enclosed. \n", data), 0);
+	if (!count_char(data))
+		return (map_error("Error : Invalid numbers of players, exits or collectibles.\n", data), 0);
+	if (!validate_char(data->map))
+		return (map_error("Error : Map contains invalid characters.\n", data), 0);
+	return (1);
 }
---
+++ src/check_map_utils.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmafueni <jmafueni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 12:03:29 by jmafueni          #+#    #+#             */
+/*   Updated: 2024/08/09 15:30:15 by jmafueni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	floodfill(char **map, int x, int y, t_game *game)
+{
+	if (x < 0 || y < 0 || !map[y] || !map[y][x] ||
+		x >= (int)strlen(map[y]) || map[y][x] == '1' || map[y][x] == 'V')
+		return ;
+	if (map[y][x] == 'E')
+		{
+			game->found_exit = 1;
+			return ;
+		}
+	if (map[y][x] == 'C')
+		game->found_item++;
+	if (map[y][x] != 'P')
+		map[y][x] = 'V';
+
+	floodfill(map, x + 1, y, game);
+	floodfill(map, x - 1, y, game);
+	floodfill(map, x, y + 1, game);
+	floodfill(map, x, y - 1, game);
+}
+
+int	find_player_position(t_data *data)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (data->map[y])
+	{
+		x = 0;
+		while (data->map[y][x])
+		{
+			if (data->map[y][x] == 'P')
+			{
+				data->game->player_x = x;
+				data->game->player_y = y;
+				return (1);
+			}
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
+char	**initialize_tmp_map(t_data *data)
+{
+	int		i;
+	char	**tmp_map;
+
+	tmp_map = (char **)malloc(sizeof(char *) * (data->game->rows + 1));
+	if (!tmp_map)
+		return (NULL);
+	i = 0;
+	while (i < data->game->rows)
+	{
+		tmp_map[i] = ft_strdup(data->map[i]);
+		if (!tmp_map[i])
+		{
+			ft_free(tmp_map);
+			return (NULL);
+		}
+		i++;
+	}
+	tmp_map[data->game->rows] = NULL;
+	return (tmp_map);
+}
+
+int	check_path(t_data *data)
+{
+	char	**tmp_map;
+
+	if (!find_player_position(data))
+		return (0);
+	data->game->found_exit = 0;
+	data->game->found_item = 0;
+	tmp_map = initialize_tmp_map(data);
+	if (!tmp_map)
+		return (0);
+	floodfill(tmp_map, data->game->player_x, data->game->player_y, data->game);
+	ft_free(tmp_map);
+	return (data->game->found_exit == 1 && data->game->found_item == data->game->total_item);
+}
